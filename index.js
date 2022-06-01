25b91f4492fc73484386c226130aa0b28f59b827
jharahulk <s1032190307@gmail.com>
2022-06-01 16:41:51 +0530

    Initial FOlder Strucuture Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// require express library
+const express = require("express")
+// acquire express functinality
+const app = express();
+// define port
+const port = 8000;
+
+// server listening on port 8000
+app.listen(port, (err)=>{
+    // if error display it
+    if(err)
+        console.log(`Error in runnning Server`);
+    // if not error display listenning message
+    console.log(`Server is running on Port ${port}`);
+}) 
