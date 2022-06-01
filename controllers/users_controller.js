ae84d512c13228f4779bc352ce9db70eab6cec00
rjha27404 <rjha27404@gmail.com>
2022-06-01 18:36:44 +0530

    Updated config models and views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,9 @@
 module.exports.profile = (req,res)=>{
-    return res.end("<h1>User Controller</h1>");
+    // return res.end("<h1>User Controller</h1>");
+    return res.render('user_profile',{
+        title:"Users",
+        name:"Profile"
+    })
 }
 // post contreoller
 module.exports.post = (req,res)=>{
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 // require express library
 const express = require("express")
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+const db = require('./config/mongoose');
+
 // acquire express functinality
 const app = express();
 // define port
 const port = 8000;
-
+// use view engine
+app.set('view engine', 'ejs');
+// path setting
+app.set('views',path.join(__dirname,'views'));
 // use express router
 app.use('/',require('./routes'));
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // server listening on port 8000
 app.listen(port, (err)=>{
